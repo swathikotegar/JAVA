31b6808b9159a5949d1415bf1fd043f1fa0f9eab
swathikotegar <84147127+swathikotegar@users.noreply.github.com>
2021-07-22 13:18:40 -0700

    To print Name, Year of joining, Address
    it prints the Name ,Year of joining ,Address with class Employee.

---
+++ Employee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee;
+
+/**
+ *
+ * @author Shamili
+ */
+public class Employee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Name Year of joining Address");
+
+System.out.println("Robert 1994 64C- WallsStreet");
+
+System.out.println("Sam 2000 68D- WallsStreet");
+
+System.out.println("John 1999 26B- WallsStreet");
+        
+    }
+    
+}
